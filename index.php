793479b7ac3d08bcd8af7bb2c7896157b821b5d6
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2025-08-18 15:00:58 +0100

    CAROUSEL DESIGN

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
     <title>Xander Hotel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 
@@ -10,15 +10,15 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
     <style>
-        *{
+        * {
             font-family: 'Poppins', sans-serif;
         }
-        .h-font{
+        .h-font {
             font-family: 'Merienda', cursive;
         }
-        /* Removes arrows and spin from inputs */
         input::-webkit-outer-spin-button,
         input::-webkit-inner-spin-button {
             -webkit-appearance: none;
@@ -27,10 +27,30 @@
         input[type=number] {
             -moz-appearance: textfield;
         }
+        html, body {
+            position: relative;
+            height: 100%;
+            margin: 0;
+            padding: 0;
+            font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+            font-size: 14px;
+            color: #fff;
+        }
+        .swiper {
+            width: 100%;
+            height: 100%;
+        }
+        .swiper-slide {
+            background-position: center;
+            background-size: cover;
+        }
+        .swiper-slide img {
+            display: block;
+            width: 100%;
+        }
     </style>
-
 </head>
-<body>
+<body class="bg-light" >
     <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
         <div class="container-fluid">
             <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">Xander Hotel</a>
@@ -54,25 +74,16 @@
                     <li class="nav-item">
                         <a class="nav-link" href="#">About</a>
                     </li>
-                    
                 </ul>
                 <div class="d-flex" role="search">
-                    <!-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/> -->
-                    <!-- <button class="btn btn-outline-success" type="submit">Search</button> -->
-                    
-                    <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
-                        Login
-                    </button>
-                    <button type="button" class="btn btn-outline-dark shadow-none" data-bs-toggle="modal" data-bs-target="#registerModal">
-                        Register
-                    </button>
+                    <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
+                    <button type="button" class="btn btn-outline-dark shadow-none" data-bs-toggle="modal" data-bs-target="#registerModal">Register</button>
                 </div>
             </div>
         </div>
     </nav>
 
-    <!-- Modal -->
+    <!-- Login Modal -->
     <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
@@ -85,18 +96,16 @@
                     </div>
                     <div class="modal-body">
                         <div class="mb-3">
-                            <label for="form-label">Email address</label>
-                            <input type="email" class="form-control shadow-none">
+                            <label for="email">Email address</label>
+                            <input type="email" class="form-control shadow-none" id="email" required>
                         </div>
                         <div class="mb-4">
-                            <label for="form-label">Password</label>
-                            <input type="password" class="form-control shadow-none">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control shadow-none" id="password" required>
                         </div>
                         <div class="d-flex align-items-center justify-content-between mb-2">
-                            <button type="submit" class="btn btn-dark shadow-none" >
-                                LOGIN
-                            </button>
-                            <a href="javascript: void(0)" class="text-secondary text-decoration-none" >Forgot Password ?</a>
+                            <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
+                            <a href="javascript:void(0)" class="text-secondary text-decoration-none">Forgot Password?</a>
                         </div>
                     </div>
                 </form>   
@@ -104,6 +113,7 @@
         </div>
     </div>
 
+    <!-- Registration Modal -->
     <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
@@ -115,64 +125,98 @@
                         <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                    <span class="badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base">
-                        Note: Your details must match with your ID (ID card, passport, driving license, etc.) 
-                        that will be required during check-in.
-                    </span>
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-md-6 ps-0 mb-3">
-                                <label for="form-label">Name</label>
-                                <input type="text" class="form-control shadow-none">
+                        <span class="badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base">
+                            Note: Your details must match with your ID (ID card, passport, driving license, etc.) that will be required during check-in.
+                        </span>
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-md-6 ps-0 mb-3">
+                                    <label for="name">Name</label>
+                                    <input type="text" class="form-control shadow-none" id="name" required>
+                                </div>
+                                <div class="col-md-6 p-0 mb-3">
+                                    <label for="reg-email">Email</label>
+                                    <input type="email" class="form-control shadow-none" id="reg-email" required>
+                                </div>
+                                <div class="col-md-6 ps-0 mb-3">
+                                    <label for="phone">Phone Number</label>
+                                    <input type="number" class="form-control shadow-none" id="phone" required>
+                                </div>
+                                <div class="col-md-6 p-0 mb-3">
+                                    <label for="picture">Picture</label>
+                                    <input type="file" class="form-control shadow-none" id="picture">
+                                </div>
+                                <div class="col-md-12 p-0 mb-3">
+                                    <label for="address">Address</label>
+                                    <textarea class="form-control shadow-none" id="address" rows="1" required></textarea>
+                                </div>
+                                <div class="col-md-6 ps-0 mb-3">
+                                    <label for="pin-code">Pin Code</label>
+                                    <input type="number" class="form-control shadow-none" id="pin-code" required>
+                                </div>
+                                <div class="col-md-6 p-0 mb-3">
+                                    <label for="dob">Date of Birth</label>
+                                    <input type="date" class="form-control shadow-none" id="dob" required>
+                                </div>
+                                <div class="col-md-6 ps-0 mb-3">
+                                    <label for="reg-password">Password</label>
+                                    <input type="password" class="form-control shadow-none" id="reg-password" required>
+                                </div>
+                                <div class="col-md-6 p-0 mb-3">
+                                    <label for="confirm-password">Confirm Password</label>
+                                    <input type="password" class="form-control shadow-none" id="confirm-password" required>
+                                </div>
                             </div>
-                            <div class="col-md-6 p-0 mb-3">
-                                <label for="form-label">Email</label>
-                                <input type="email" class="form-control shadow-none">
+                            <div class="text-center my-1">
+                                <button type="submit" class="btn btn-dark shadow-none">REGISTER</button>
                             </div>
-                            <div class="col-md-6 ps-0 mb-3">
-                                <label for="form-label">Phone Number</label>
-                                <input type="number" class="form-control shadow-none">
-                            </div>
-                            <div class="col-md-6 p-0 mb-3">
-                                <label for="form-label">Picture</label>
-                                <input type="file" class="form-control shadow-none">
-                            </div>
-                            <div class="col-md-12 p-0 mb-3">
-                                <label for="form-label">Address</label>
-                                <textarea class="form-control shadow-none" rows="1"></textarea>
-                            </div>
-                            <div class="col-md-6 ps-0 mb-3">
-                                <label for="form-label">Pin Code</label>
-                                <input type="number" class="form-control shadow-none">
-                            </div>
-                            <div class="col-md-6 p-0 mb-3">
-                                <label for="form-label">Date of birth</label>
-                                <input type="date" class="form-control shadow-none">
-                            </div>
-                            <div class="col-md-6 ps-0 mb-3">
-                                <label for="form-label">Password</label>
-                                <input type="password" class="form-control shadow-none">
-                            </div>
-                            <div class="col-md-6 p-0 mb-3">
-                                <label for="form-label">Confirm Password</label>
-                                <input type="password" class="form-control shadow-none">
-                            </div>
-                           
-                        </div>
-                        <div class="text-center my-1">
-                            <button type="submit" class="btn btn-dark shadow-none">
-                                REGISTER
-                            </button>
-                        </div>
-                    </div>    
-
+                        </div>    
+                    </div>
                 </form>   
             </div>
         </div>
     </div>
 
-    
+    <!-- Swiper -->
+    <div class="container-fluid px-lg-4 mt-4">
+        <div class="swiper swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <img src="images/carousel/IMG_1.png" class="w-100 d-block" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="images/carousel/IMG_2.png" class="w-100 d-block" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="images/carousel/IMG_3.png" class="w-100 d-block" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="images/carousel/IMG_4.png" class="w-100 d-block" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="images/carousel/IMG_5.png" class="w-100 d-block" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="images/carousel/IMG_6.png" class="w-100 d-block" />
+                </div>
+            </div>
+            
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+    <script>
+        var swiper = new Swiper(".swiper-container", {
+            spaceBetween: 30,
+            effect: "fade",
+            loop: true,
+            autoplay: {
+                delay: 3500,
+                disableOnInteraction: false,
+            },
+        });
+    </script>
 </body>
 </html>
