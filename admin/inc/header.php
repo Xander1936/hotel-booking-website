de2eb3da3ac34d24cb8690a42795bae22ac83548
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2025-09-22 23:48:41 +0100

    Settings Page - Part 1 Good Step

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -1,35 +1,60 @@
-<div class="container-fluid bg-dark text-light p-3 d-flex align-items-center justify-content-between sticky-top">
-        <h3 class="mb-0 h-font">Xander Hotel</h3>
-        <a href="../admin/logout.php" class="btn btn-danger btn-sm">Logout</a>
-    </div>
-
-    <div class="d-flex">
-        <div class="col-lg-2 bg-dark border-top border-3 border-secondary" id="dashboard-menu">
-            <nav class="navbar navbar-expand-lg navbar-dark">
-                <div class="container-fluid flex-lg-column align-items-stretch" style="height:100%; ">
-                    <h4 class="mt-2 text-light">ADMIN PANEL</h4>
-                    <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#adminDropdown" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse flex-column align-items-stretch mt-2" id="adminDropdown">
-                        <ul class="nav nav-pills flex-column">
-                            <li class="nav-item">
-                                <a href="../admin/dashboard.php" class="nav-link text-white">Dashboard</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link text-white">Rooms</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link text-white">Users</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link text-white">Settings</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+        <div class="container-fluid bg-dark text-light p-3 d-flex align-items-center justify-content-between sticky-top">
+            <h3 class="navbar-brand me-5 fw-bold fs-3 h-font mb-0">Xander Hotel</h3>
+            <a href="../admin/logout.php" class="btn btn-danger btn-sm">Logout</a>
         </div>
-    <?php require("/admin/inc/main_content.php"); ?>
-    </div>
+
+        <div class="d-flex">
+            <!-- <div class="col-lg-2 bg-dark border-top border-3 border-secondary" id="dashboard-menu" style="height: 100vh;">
+                <nav class="navbar navbar-expand-lg navbar-dark flex-column" style="height: 100%;">
+                    <div class="container-fluid flex-lg-column align-items-stretch h-100" style="height: 100%;">
+                        <h4 class="mt-2 text-light">ADMIN PANEL</h4>
+                        <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#adminDropdown" aria-controls="adminDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse flex-column align-items-stretch mt-2" id="adminDropdown">
+                            <ul class="nav nav-pills flex-column">
+                                <li class="nav-item">
+                                    <a href="../admin/dashboard.php" class="nav-link text-white">Dashboard</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link text-white">Rooms</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link text-white">Users</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="settings.php" class="nav-link text-white">Settings</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>     -->
+
+            <div class="col-lg-2 bg-dark border-top border-3 border-secondary" id="dashboard-menu" style="height: 100vh;">
+                <nav class="navbar navbar-expand-lg navbar-dark flex-column" style="height: 100%;">
+                    <div class="container-fluid flex-lg-column align-items-stretch h-100" style="height: 100%;">
+                        <h4 class="mt-2 text-light">ADMIN PANEL</h4>
+                        <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#adminDropdown" aria-controls="adminDropdown" aria-expanded="false" aria-label="Toggle navigation" >
+                            <span class="navbar-toggler-icon" style="width: 30px; height: 20px;"></span>
+                        </button>
+                        <div class="collapse navbar-collapse flex-column align-items-stretch mt-2" id="adminDropdown">
+                            <ul class="nav nav-pills flex-column">
+                                <li class="nav-item">
+                                    <a href="../admin/dashboard.php" class="nav-link text-white">Dashboard</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link text-white">Rooms</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link text-white">Users</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="settings.php" class="nav-link text-white">Settings</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
 
---
+++ admin/settings.php
@@ -0,0 +1,23 @@
+<?php 
+    require('../admin/inc/essentials.php'); 
+    require("../admin/inc/scripts.php"); 
+    adminLogin(); 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Admin Panel - Settings</title>
+        <?php require("../admin/inc/links.php"); ?>
+    </head>
+
+    <body class="bg-light">
+
+        <?php require("../admin/inc/header.php"); ?>    
+        <?php require("../admin/inc/main_content.php"); ?>
+
+    </body>
+</html>
